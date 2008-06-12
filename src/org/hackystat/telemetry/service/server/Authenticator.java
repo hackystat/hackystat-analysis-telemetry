28a5072412f246d18c84abdda9a69c59ceedaa94
Philip Johnson <philipmjohnson@gmail.com>
2008-06-12 18:51:16 +0000

    Addressing thread safety issues in DPDClient code.

--- src/org/hackystat/telemetry/service/server/Authenticator.java
+++ src/org/hackystat/telemetry/service/server/Authenticator.java
@@ -24,18 +24,18 @@ import org.restlet.data.ChallengeScheme;
  * @author Philip Johnson
  */
 public class Authenticator extends Guard {
-  
-  /** A map containing previously verified credentials. */
-  private Map<String, String> credentials = new HashMap<String, String>();
-  
-  /** A map containing DailyProjectDataClient instances, one per credentialled user. */
-  private Map<String, DailyProjectDataClient> dpdMap = 
+
+  /** A singleton map containing previously verified credentials. */
+  private static Map<String, String> credentials = new HashMap<String, String>();
+
+  /** A singleton map containing DailyProjectDataClient instances, one per credentialled user. */
+  private static Map<String, DailyProjectDataClient> dpdClientMap = 
     new HashMap<String, DailyProjectDataClient>();
-  
-  /** A map containing SensorBaseClient instances, one per credentialled user. */
-  private Map<String, SensorBaseClient> sensorbaseMap = 
+
+  /** A singleton map containing SensorBaseClient instances, one per credentialled user. */
+  private static Map<String, SensorBaseClient> sensorBaseClientMap = 
     new HashMap<String, SensorBaseClient>();
-   
+
   /** The sensorbase host, such as "http://localhost:9876/sensorbase/" */
   private String sensorBaseHost;
 
@@ -50,56 +50,68 @@ public class Authenticator extends Guard {
     "authenticator.sensorbase.clients";
 
   /**
-   * Initializes this Guard to do HTTP Basic authentication.
-   * Puts the credentials map in the server context so that Resources can get the 
-   * password associated with the uriUser for their own invocations to the SensorBase. 
+   * Initializes this Guard to do HTTP Basic authentication. Puts the credentials map in the server
+   * context so that Resources can get the password associated with the uriUser for their own
+   * invocations to the SensorBase.
+   * 
    * @param context The server context.
    * @param sensorBaseHost The sensorbase service, such as 'http://localhost:9876/sensorbase/'.
    * @param dpdHost The DPD service, such as 'http://localhost:9877/dailyprojectdata/'.
    */
-  public Authenticator (Context context, String sensorBaseHost, String dpdHost) {
-    super(context, ChallengeScheme.HTTP_BASIC,  "DailyProjectData");
+  public Authenticator(Context context, String sensorBaseHost, String dpdHost) {
+    super(context, ChallengeScheme.HTTP_BASIC, "DailyProjectData");
     this.sensorBaseHost = sensorBaseHost;
     this.dpdHost = dpdHost;
-    context.getAttributes().put(AUTHENTICATOR_DPD_CLIENTS_KEY, dpdMap);
-    context.getAttributes().put(AUTHENTICATOR_SENSORBASE_CLIENTS_KEY, sensorbaseMap);
+    context.getAttributes().put(AUTHENTICATOR_DPD_CLIENTS_KEY, dpdClientMap);
+    context.getAttributes().put(AUTHENTICATOR_SENSORBASE_CLIENTS_KEY, sensorBaseClientMap);
   }
-  
+
   /**
    * Returns true if the passed credentials are OK.
+   * 
    * @param identifier The account name.
-   * @param secretCharArray The password. 
+   * @param secretCharArray The password.
    * @return If the credentials are valid.
    */
-  @Override protected boolean checkSecret(String identifier, char[] secretCharArray) {
-    String secret = new String(secretCharArray);
-    // Return true if the user/password credentials are in the cache. 
-    if (credentials.containsKey(identifier) &&
-        secret.equals(credentials.get(identifier))) {
-      return true;
-    }
-    // Otherwise we check the credentials with the SensorBase.
-    boolean isRegistered = SensorBaseClient.isRegistered(sensorBaseHost, identifier, secret);
-    if (isRegistered) {
-      // Credentials are good, so save them and create clients for this user. 
-      credentials.put(identifier, secret);
-      // Only create a new client if there is no old one. This is intended to facilitate caching,
-      // but has the problem that there's no way to get rid of bogus entries besides restarting
-      // this service.  Something to worry about. 
-      if (!dpdMap.containsKey(identifier)) {
-        Server server = (Server)getContext().getAttributes().get("TelemetryServer");
-        ServerProperties props = server.getServerProperties();
-        DailyProjectDataClient client = new DailyProjectDataClient(dpdHost, identifier, secret); 
-        if (props.isCacheEnabled()) { //NOPMD
-          client.enableCaching(identifier, "telemetry", 
-              props.getCacheMaxLife(), props.getCacheCapacity());
+  @Override
+  protected boolean checkSecret(String identifier, char[] secretCharArray) {
+    /*
+     * I am synchronizing here on a static (class-wide) variable for two reasons: (1) JCS
+     * write-through caching fails when multiple threads access the same back-end file:
+     * <https://issues.apache.org/jira/browse/JCS-31>. Thus, it is vitally important to ensure that
+     * only one instance of a DPDClient for any given user is created. (2) I do not know if
+     * Restlet allows multiple Authenticator instances. Thus, I am synchronizing on a class-wide
+     * variable just in case. This synchronization creates a bottleneck on every request, but the
+     * benefits of reliable caching should outweigh this potential performance hit under high loads.
+     */
+    synchronized (dpdClientMap) {
+      String secret = new String(secretCharArray);
+      // Return true if the user/password credentials are in the cache.
+      if (credentials.containsKey(identifier) && secret.equals(credentials.get(identifier))) {
+        return true;
+      }
+      // Otherwise we check the credentials with the SensorBase.
+      boolean isRegistered = SensorBaseClient.isRegistered(sensorBaseHost, identifier, secret);
+      if (isRegistered) {
+        // Credentials are good, so save them and create clients for this user.
+        credentials.put(identifier, secret);
+        // Only create a new client if there is no old one. 
+        if (!dpdClientMap.containsKey(identifier)) {
+          Server server = (Server) getContext().getAttributes().get("TelemetryServer");
+          ServerProperties props = server.getServerProperties();
+          DailyProjectDataClient client = new DailyProjectDataClient(dpdHost, identifier, secret);
+          if (props.isCacheEnabled()) { // NOPMD
+            client.enableCaching(identifier, "telemetry", props.getCacheMaxLife(), props
+                .getCacheCapacity());
+          }
+          dpdClientMap.put(identifier, client);
         }
-        dpdMap.put(identifier, client);
+        if (!sensorBaseClientMap.containsKey(identifier)) {
+          SensorBaseClient client = new SensorBaseClient(sensorBaseHost, identifier, secret);
+          sensorBaseClientMap.put(identifier, client);
+        }
       }
-      if (!sensorbaseMap.containsKey(identifier)) {
-        sensorbaseMap.put(identifier, new SensorBaseClient(sensorBaseHost, identifier, secret));
-      }
+      return isRegistered;
     }
-    return isRegistered;
   }
 }
